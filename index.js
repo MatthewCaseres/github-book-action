f0b0dc615524ba0f50c2ce9be66ab2e22cad97de
Matthew Caseres <matthewcaseres@outlook.com>
2021-05-13 17:14:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const {writeFile} = require('fs/promises');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  await writeFile('lol.txt', 'haha')
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+main().catch((error) => core.setFailed(error.message));
+
+async function main() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    await writeFile('lol.txt', 'haha')
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }

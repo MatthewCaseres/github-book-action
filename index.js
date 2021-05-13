45b90058e81cb71a9087636a0a794868208486ca
Matthew Caseres <matthewcaseres@outlook.com>
2021-05-13 17:11:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const {writeFile} = require('fs/promises');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -7,6 +8,7 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+  await writeFile('lol.txt', 'haha')
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
